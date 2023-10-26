5d932e8a09c1edf87397a35d1ebd0960c19712c6
Lion~ Bernhard <stbethom@stud.h-da.de>
2023-10-26 08:45:44 +0200

    Begin implementation of backend and database

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,4 @@
-use sqlx::postgres::PgPoolOptions;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
 const DATABASE_URL: &str = "postgresql://Driver:1234@localhost/Drivers";
 
@@ -9,4 +9,45 @@ async fn main() {
         .connect(DATABASE_URL)
         .await
         .unwrap();
+
+    let chris = Driver::new("Chris", "1234");
+    create_driver(pool.clone(), &chris).await.unwrap();
+}
+
+async fn create_driver(
+    pool: Pool<Postgres>,
+    driver: &Driver,
+) -> Result<sqlx::postgres::PgQueryResult, sqlx::Error> {
+    sqlx::query!(
+        "INSERT INTO DRIVER (name, password)
+        VALUES ($1, $2)",
+        driver.name,
+        driver.password
+    )
+    .execute(&pool)
+    .await
+}
+
+struct Driver {
+    name: String,
+    password: String,
+}
+
+impl Driver {
+    pub fn new(name: impl Into<String>, password: impl Into<String>) -> Self {
+        Self {
+            name: name.into(),
+            password: password.into(),
+        }
+    }
+}
+
+struct UserManager {
+    database: Pool<Postgres>,
+}
+
+impl UserManager {
+    pub fn new(database: Pool<Postgres>) -> Self {
+        Self { database }
+    }
 }
