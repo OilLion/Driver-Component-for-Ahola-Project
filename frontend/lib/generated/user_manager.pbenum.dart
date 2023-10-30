a531123b670b1fb32f5ac96084213ca3b7781a38
Christian Schmitt <christian.f.schmitt@stud.h-da.de>
2023-10-30 12:24:50 +0100

    added grpc files

---
+++ frontend/lib/generated/user_manager.pbenum.dart
@@ -0,0 +1,53 @@
+//
+//  Generated code. Do not modify.
+//  source: user_manager.proto
+//
+// @dart = 2.12
+
+// ignore_for_file: annotate_overrides, camel_case_types, comment_references
+// ignore_for_file: constant_identifier_names, library_prefixes
+// ignore_for_file: non_constant_identifier_names, prefer_final_fields
+// ignore_for_file: unnecessary_import, unnecessary_this, unused_import
+
+import 'dart:core' as $core;
+
+import 'package:protobuf/protobuf.dart' as $pb;
+
+class RegistrationResult extends $pb.ProtobufEnum {
+  static const RegistrationResult RegistrationSuccess = RegistrationResult._(0, _omitEnumNames ? '' : 'RegistrationSuccess');
+  static const RegistrationResult UserAlreadyExists = RegistrationResult._(1, _omitEnumNames ? '' : 'UserAlreadyExists');
+  static const RegistrationResult RegistrationUnknownError = RegistrationResult._(-1, _omitEnumNames ? '' : 'RegistrationUnknownError');
+
+  static const $core.List<RegistrationResult> values = <RegistrationResult> [
+    RegistrationSuccess,
+    UserAlreadyExists,
+    RegistrationUnknownError,
+  ];
+
+  static final $core.Map<$core.int, RegistrationResult> _byValue = $pb.ProtobufEnum.initByValue(values);
+  static RegistrationResult? valueOf($core.int value) => _byValue[value];
+
+  const RegistrationResult._($core.int v, $core.String n) : super(v, n);
+}
+
+class LoginResult extends $pb.ProtobufEnum {
+  static const LoginResult LoginSuccess = LoginResult._(0, _omitEnumNames ? '' : 'LoginSuccess');
+  static const LoginResult InalidPassword = LoginResult._(1, _omitEnumNames ? '' : 'InalidPassword');
+  static const LoginResult DoesNotExist = LoginResult._(2, _omitEnumNames ? '' : 'DoesNotExist');
+  static const LoginResult LoginUnknownError = LoginResult._(-1, _omitEnumNames ? '' : 'LoginUnknownError');
+
+  static const $core.List<LoginResult> values = <LoginResult> [
+    LoginSuccess,
+    InalidPassword,
+    DoesNotExist,
+    LoginUnknownError,
+  ];
+
+  static final $core.Map<$core.int, LoginResult> _byValue = $pb.ProtobufEnum.initByValue(values);
+  static LoginResult? valueOf($core.int value) => _byValue[value];
+
+  const LoginResult._($core.int v, $core.String n) : super(v, n);
+}
+
+
+const _omitEnumNames = $core.bool.fromEnvironment('protobuf.omit_enum_names');
