f18e033b8b048783656f4bbd01cabc98d237f44f
stchscm2 <christian.f.schmitt@stud.h-da.de>
2023-11-26 09:52:09 +0100

    added status Updater client

--- frontend/lib/client.dart
+++ frontend/lib/client.dart
@@ -1,6 +1,6 @@
 import 'package:grpc/grpc.dart';
 import 'package:frontend/generated/user_manager.pbgrpc.dart';
-
+import 'package:frontend/generated/status_updater.pbgrpc.dart';
 import 'generated/route_manager.pbgrpc.dart';
 
 class UserManagerService {
@@ -91,33 +91,46 @@ class RouteManagerService {
   }
 }
 
+class StatusUpdaterService {
 
+  ///here enter your host without the http part (e.g enter google.com now http://google.com)
+  String baseUrl = "10.0.2.2";
 
+  StatusUpdaterService._internal();
+  static final StatusUpdaterService _instance = StatusUpdaterService._internal();
 
+  factory StatusUpdaterService() => _instance;
 
+  ///static HelloService instance that we will call when we want to make requests
+  static StatusUpdaterService get instance => _instance;
+  ///HelloClient is the  class that was generated for us when we ran the generation command
+  ///We will pass a channel to it to intialize it
+  late DriverUpdaterClient _statusUpdaterClient;
 
+  ///this will be used to create a channel once we create this class.
+  ///Call HelloService().init() before making any call.
+  Future<void> init() async {
+    _createChannel();
+  }
 
+  ///provide public access to the HelloClient instance
+  DriverUpdaterClient get statusUpdaterClient {
+    return _statusUpdaterClient;
+  }
 
+  ///here we create a channel and use it to initialize the HelloClient that was generated
+  ///
+  _createChannel() {
+    final channel = ClientChannel(
+      baseUrl,
+      port: 4269,
 
+      ///use credentials: ChannelCredentials.insecure() if you want to connect without Tls
+      options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
 
-
-
-
-/*import 'package:frontend/generated/user_manager.pbgrpc.dart';
-import 'package:grpc/grpc.dart';
-
-
-void main() async {
-  final channel = ClientChannel(
-    'localhost',
-    port: 4269,
-    options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
-  );
-
-  final stub = UserManagerClient(channel);
-
-  var response = await stub.loginUser(Login());
-  print('Response received: ${response}');
-
-  await channel.shutdown();
-}*/
+      ///use this if you are connecting with Tls
+      //options: const ChannelOptions(),
+    );
+    _statusUpdaterClient = DriverUpdaterClient(channel);
+  }
+}
