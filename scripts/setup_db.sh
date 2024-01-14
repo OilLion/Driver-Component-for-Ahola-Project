e8b61d335ea7dba5295ee0b5ee46151406016d74
Lion~ Bernhard <stbethom@stud.h-da.de>
2024-01-14 10:02:40 +0100

    Updated help output for setup_db.sh

--- scripts/setup_db.sh
+++ scripts/setup_db.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 print_help () {
-  printf "Script for replacing the database specified in DB_NAME with the\n%b\
-database specified in the init.sql file in the SQL_INIT_DIR.\n"
-  printf "Usage:\nupdate_sql.sh <CONTAINER_NAME> <USER_NAME> <DB_NAME> <SQL_INIT_FILE>\n"
-  printf "Example:\nupdate_sql.sh driver_database Driver Drivers ./database/init.sql\n"
+  printf "Script for setting up the database specified in DB_NAME with the scripts is the specified directory SQL_FILE_DIR.\n"
+  printf "\033[1;37m\033[4;37mUsage:\n\033[0m    update_sql.sh <CONTAINER_NAME> <USER_NAME> <DB_NAME> <SQL_FILE_DIR>\n"
+  printf "\033[1;37m\033[4;37mExample:\n\033[0m    update_sql.sh driver_database Driver Drivers ./database/init.sql\n"
 }
 
 if [[ "$1" = "--help" || "$#" -ne "4" ]]; then
@@ -17,8 +16,6 @@ else
 
   docker exec $container dropdb -f -U $user $database
   docker exec $container createdb -U $user $database
-  docker exec $container touch sql_file.sql
-  docker exec $container sh -c "echo '`cat $sql_dir/init.sql`' > sql_init.sql"
   docker cp $sql_dir $container:setup_files
   docker exec $container psql -U $user -d $database -f setup_files/init.sql
   docker exec $container psql -U $user -d $database -f setup_files/data.sql
